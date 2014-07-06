442db55ee287c5c8e667248dd2a56d017a40a421
Yen Tran <tran.yentran@gmail.com>
2014-07-06 16:11:03 -0700

    added my solutions

--- week-8-and-9/3-Ruby/triangle-debugger/my_solution.rb
+++ week-8-and-9/3-Ruby/triangle-debugger/my_solution.rb
@@ -1,36 +1,37 @@
 # U3.W8-9: Triangle Debugger
 
 
-# I worked on this challenge with Yen: ].
+# I worked on this challenge with Clawson Cassidy
 
 
 # 1. Original Solution
 
-def valid_triangle?(a, b, c)
-  sum = 0
-  if a != 0 || b != 0 || c != 0
-    if a >= b
-      largest = a
-      sum += b
-    else 
-      largest = b
-      sum += a
-    end
-    if c > largest
-      sum += largest
-      largest = c
-    else sum += c
-    end
-    
-    if sum > largest
-      return "true"
-    else return "false"
-    end
-  else return "false"
-  end
-end
+# def valid_triangle?(a, b, c)
+#   sum = 0
+#   if a != 0 || b != 0 || c != 0
+#     if a >= b
+#       largest = a
+#       sum += b
+#     else 
+#       largest = b
+#       sum += a
+#     end
 
-puts valid_triangle?(100, 100, 1)
+#     if c > largest
+#       sum += largest
+#       largest = c
+#     else sum += c
+#     end
+    
+#     if sum > largest
+#       return "true"
+#     else return "false"
+#     end
+#   else return "false"
+#   end
+# end
+
+
 
 
 # 2. Answer the question for each bug
@@ -70,14 +71,22 @@ puts valid_triangle?(100, 100, 1)
 
 # 3. Refactored Solution (Comment the other code to run this)
 
+def valid_triangle?(*triangle)
+  return false if triangle.sort!.length != 3
+  triangle.each { |side| return false if (side <=> 0) <= 0 }
+  return false if (triangle[0] + triangle[1]) <= triangle[2]
+  return true
+end
 
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
-
-
+puts valid_triangle?(100, 100, 1)
 
 
 
 
 # 5. Reflection 
+=begin 
+I didn't feel comfortable with this challenge and really needed to lean on my pair for this exercise. In the refactoring solution my partner suggested to use the combined comparison operator, which I completely forgot from when I did it in Codeacademy. I think what I'm stuggling with is that I am forgetting a lot of what I've learned in the past couple of weeks. Neet to start putting together those cheetsheets to recall...
+=end
