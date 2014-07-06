1b5a4cebf7f6cb6be6bc47a3753bd4cdda16b1f6
Yen Tran <tran.yentran@gmail.com>
2014-07-06 13:12:12 -0700

    adding current solutions

--- week-8-and-9/3-Ruby/triangle-debugger/my_solution.rb
+++ week-8-and-9/3-Ruby/triangle-debugger/my_solution.rb
@@ -1,48 +1,70 @@
 # U3.W8-9: Triangle Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Yen: ].
 
 
 # 1. Original Solution
 
-def valid_triangle?(a, b, c, sum)
+def valid_triangle?(a, b, c)
+  sum = 0
   if a != 0 || b != 0 || c != 0
-  if a >= b
-  largest = a
-  sum += b
-  else largest = b
-  sum += a
-  end
-  if c > largest
-  sum += largest
-  largest = c
-  else sum += c
-  end
-  if sum > largest
-  return "true"
-  else return "false"
-  end
+    if a >= b
+      largest = a
+      sum += b
+    else 
+      largest = b
+      sum += a
+    end
+    if c > largest
+      sum += largest
+      largest = c
+    else sum += c
+    end
+    
+    if sum > largest
+      return "true"
+    else return "false"
+    end
   else return "false"
   end
 end
+
+puts valid_triangle?(100, 100, 1)
 
 
 # 2. Answer the question for each bug
 
 # --- Bug 1 ---
 # * what is the exact description of the error?
+# valid_triangle? expects 3 arguments
+
 # * what is the exact line number the error is appearing?
+# Line 9
 # * before you fix the bug, what do you think is causing the error?
+# It was expecting 3 arguments but got 4. We created a new sum variable and set it to 0. 
 
 # --- Bug 2 ---
 # * what is the exact description of the error?
+# valid_triangle? returns true for an equilateral triangle
+# Failure/Error: valid_triangle?(length, length, length).should be_true
+#   expected true to respond to `true?`
+
 # * what is the exact line number the error is appearing?
+# ./triangle_spec.rb:14:in `block (2 levels) in <top (required)>'
+
 # * before you fix the bug, what do you think is causing the error?
+# Because a is not bigger than b, and b is not bigger than c. Doesn't pass both condition. 
 
 # --- Bug 3 ---
 # * what is the exact description of the error?
+# valid_triangle? returns true for an isoceles triangle
+#      Failure/Error: isoceles_triangles.all? do |triangle|
+#        expected true to respond to `true?`
+
 # * what is the exact line number the error is appearing?
+# ./triangle_spec.rb:32:in `block (2 levels) in <top (required)>
+
 # * before you fix the bug, what do you think is causing the error?
 
 
