ef9abadbee17cf20fac4d31c17bc29275a71d6a6
Yen Tran <tran.yentran@gmail.com>
2014-07-06 13:11:20 -0700

    added solutions

--- week-8-and-9/2-JavaScript/jquery/jquery_example.js
+++ week-8-and-9/2-JavaScript/jquery/jquery_example.js
@@ -7,18 +7,37 @@ $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
- 
+ bodyElement = $('body')
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
- 
- 
+ h1 = $('h1')
+ h1.css('color', 'green')
+ h1.css('border', 'solid gray 3px')
+ //h1.css('visibility', 'hidden')
+ h1.html('Mule Deer')
+
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
- 
+ $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://www.nps.gov/romo/naturescience/images/mule_deer_buck_1.jpg')
+  })
+
+ $('img').on('mouseleave', function(f){
+     f.preventDefault()
+    $(this).attr('src', 'dbc_logo.jpg')
+  })
  
 //RELEASE 4 : Experiment on your own
- 
+ $('img').css('border-style', 'dotted');
+ $('img').css('border-color', 'red');
+ $('img').css('border-width', '1px');
+
+ $('img').animate({
+ 	width: "70%",
+ 	'border-width': "10px"
+ }, 2000 );
  
  
  
